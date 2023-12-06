550132de3a8100f828c9460f5529cf3547d25115
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-12-05 19:09:03 -0500

    application working PR needed for finalization

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-// App.js
-import React from 'react';
-import MyComponent from './components/MyComponent';
-import './App.css';
+//src/App.js
+import React, { useState } from 'react';
+import './styles.css';
+import ImageUploader from './components/ImageUploader';
+import LoadingInterface from './components/LoadingInterface';
+import UploadSuccessInterface from './components/UploadSuccessInterface';
 
 const App = () => {
+  const [currentInterface, setCurrentInterface] = useState(1);
+  const [uploadedFile, setUploadedFile] = useState(null);
+
+  const handleNext = (file) => {
+    setCurrentInterface(2); 
+    setTimeout(() => {
+      setUploadedFile(file);
+      setCurrentInterface(3); 
+    }, 2000); 
+  };
+
+  const renderInterface = () => {
+    switch (currentInterface) {
+      case 1:
+        return <ImageUploader onNext={handleNext} />;
+      case 2:
+        return <LoadingInterface />;
+      case 3:
+        return <UploadSuccessInterface uploadedFile={uploadedFile} />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div class="rectangle-container">
-      <input style={{ display: 'none' }} type="file" accept="image/*" />
-      <br />
-      <h2 class="title">Uploading your image</h2>
-      <br />
-      <p class="subtitle">File should be Jpeg, Png,...</p>
-      <div  className="square-container" type="file" accept="image/*" />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p class="Or">Or</p>
-      <br />
-      <button class="button">Choose a file</button>
-      <br />
-
+    <div>
+      {renderInterface()}
     </div>
-
   );
 };
 
